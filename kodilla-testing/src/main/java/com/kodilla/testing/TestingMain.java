c65ca33a7353d766d28c2508c2d6e37a8d1b03b2
bkomorniczak <b.komorniczak@gmail.com>
2020-02-13 16:11:11 +0100

    Calculator test 2 methods

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,35 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł6. Wprowadzenie do testowania oprogramowania.");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if(result.equals("theForumUser")){
+            System.out.println("Test ok.");
+        }else {
+            System.out.println("Error");
+        }
+
+        Calculator calculator = new Calculator();
+
+        int addResult = calculator.add(3,4);
+        int subResult = calculator.sub(10,4);
+
+        if(addResult==7){
+            System.out.println("Adding test ok");
+        }else{
+            System.out.println("Adding test error");
+        }
+
+        if (subResult==6){
+            System.out.println("Subtracting test ok");
+        }else {
+            System.out.println("Subtracting test error");
+        }
     }
 }
