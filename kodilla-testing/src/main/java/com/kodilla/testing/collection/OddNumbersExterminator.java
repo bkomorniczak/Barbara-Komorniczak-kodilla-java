c6c7057121aa488061d5fd99f4023eac8a34b7e0
bkomorniczak <b.komorniczak@gmail.com>
2020-02-20 14:01:48 +0100

    Exterminator fixed

--- kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -6,9 +6,13 @@ import java.util.List;
 public class OddNumbersExterminator {
 
     public static List<Integer> exterminate(List<Integer> numbers){
+        List<Integer> oddNumbers = new ArrayList <>();
 
-        numbers.removeIf(i->i%2!=0);
-
-        return numbers;
+        for(int i=0; i<numbers.size();i++) {
+            if(numbers.get(i)%2==0){
+                oddNumbers.add(numbers.get(i));
+            }
+        }
+        return oddNumbers;
     }
 }
